fb235523f4c045cb49e3e00d4c52b62b579b2a3b
omarturshan <omarturshan@outlook.com>
2025-05-01 18:58:21 +0300

    Update ektifa_bot.py

--- ektifa_bot.py
+++ ektifa_bot.py
@@ -95,7 +95,7 @@ async def main():
     print("✅ Webhook تم تفعيله:", success)
 
     # تشغيل السيرفر
-    await web_app.run_task(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    await web_app.run_task(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
 if __name__ == "__main__":
     asyncio.run(main())
